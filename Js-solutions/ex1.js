fd41b4f5aaef1e82cf757192795bd71d5203f15a
mateusoliveer <olivercorporativo90@gmail.com>
2023-10-21 15:13:15 -0300

    solve ex01

---
+++ Js-solutions/ex1.js
@@ -0,0 +1,24 @@
+
+/**
+
+ * Create two variables a and b, and initially set them each to a different number. Write a program that swaps both values.
+
+* Example: a = 10, b = 20
+* Output: a = 20, b = 10
+
+Warning! Do not use the programming language MAGIC. After you complete the exercise feel free to do so.
+
+ * 
+ */
+
+let a = 50;
+let b = 10;
+let temporaria = a; //50
+
+a = b; //10
+b = temporaria; // 50
+console.log(a,b); // 10 50
+
+
+
+
