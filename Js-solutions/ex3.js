0dd30fb7e38da59bb0fc89ce292ab2a59625a927
mateusoliveer <olivercorporativo90@gmail.com>
2023-10-21 15:46:13 -0300

    solve ex03

---
+++ Js-solutions/ex3.js
@@ -0,0 +1,25 @@
+/*
+# ex.3
+You've bought a Bitcoin and now it's on the rise!!!
+
+Create a program that:
+1) Reads the value of the bitcoin at the time of purchase
+2) Reads the percentage of increase (or decrease)
+3) Prints the total value of your bitcoin
+4) Prints the increase or decrease value
+
+* Example: bitcoin_value = 10000, bitcoin_increase = 10
+* Output: total_bitcoin_value = 11000, bitcoin_increase_value = 1000
+
+
+*/
+
+let bitcoin = 200000;
+let porcentagem = 0.3; // 30% 60MIL
+let aumento =(bitcoin*porcentagem);
+let total =(bitcoin+aumento);
+console.log(total)
+
+
+
+
