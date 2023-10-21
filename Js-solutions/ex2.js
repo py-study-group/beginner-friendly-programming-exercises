57173ec1720de825e78ba5a0aa84f6f8fa67306b
mateusoliveer <olivercorporativo90@gmail.com>
2023-10-21 15:30:34 -0300

    solve ex02

---
+++ Js-solutions/ex2.js
@@ -0,0 +1,27 @@
+/*
+# ex.2
+It's the end of the semester and you got your grades from three classes: Geometry, Algebra, and Physics.
+
+Create a program that:
+1) Reads the grades of these 3 classes (Grades range from 0 - 10)
+2) Calculate the average of your grades
+
+* Example: Geometry = 6, Algebra = 7, Physics = 8
+* Output: average_score = 7
+
+Warning! Do not use the programming language MAGIC. After you complete the exercise feel free to do so.
+
+*/
+
+// variaveis - criada as variaveis com os valores
+let Geometria = 10;
+let Algebra = 5;
+let Física = 3;
+//Somar
+let Soma =(Geometria+Algebra+Física);// A soma das 3 variaveis
+
+//dividir
+let Media =(Soma/3) // Variavel com a Media / por 3 para o resultado
+console.log(Media) 
+
+
